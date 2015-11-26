fde2a1b2502b72875f27eeccba631140f41446af
Дмитрий Антонов < >
2015-11-26 21:51:25 +0300

    add angular routing and css styles

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,12 @@
 'use strict';
 
-var app = angular.module('movies', []);
+var app = angular.module('movies', ['ngRoute']);
+
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider.
+    when('/',
+        {
+            templateUrl: 'views/movie.html',
+            controller: 'MovieController'
+        });
+})
